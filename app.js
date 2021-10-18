dbb83819b779ef6523e75b7474a55c3709e456f0
jaylamonae1 <jbillingslea@doubleedgeagency.com>
2021-10-17 23:57:47 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+const EventEmitter= require(EventEmitter());
+const emitter= new EventEmitter;
+
+// this is me learning how to commit some code to main so that I can learn how to push my code to github. 
+
